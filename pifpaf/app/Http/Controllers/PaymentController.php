ad68fe617826252d1feb088b0d0ae4d3962bff2f
Ganngann <52640533+Ganngann@users.noreply.github.com>
2025-11-04 16:31:59 +0100

    Import DB facade in PaymentController

--- pifpaf/app/Http/Controllers/PaymentController.php
+++ pifpaf/app/Http/Controllers/PaymentController.php
@@ -7,6 +7,7 @@ use App\Models\Transaction;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\DB;
 
 class PaymentController extends Controller
 {
